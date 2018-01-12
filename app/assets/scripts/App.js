d7aeeddeef65d127eb247f00d515e8ded0b8dedc
Pino <pino@giza-ip.com>
2018-01-12 15:03:02 +0100

    finished design part 1 from Header

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,13 @@
+var $ = require('jquery');
+
+var Person = require('./modules/Person');
+
+/*alert("testing ABC 123"); */
+var john = new Person("John Doe", "blue");
+john.greet();
+var jane = new Person("Jane Smith", "green");
+jane.greet();
+
+
+/*$("h1").remove();
+*/
